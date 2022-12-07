be9bb3cebf84c09e32f2316605cdaa384070db92
bikuu <bikuu@github.com>
2022-12-07 20:51:59 +0545

    Successfully run  recipe API

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '5a903661-eec0-4543-9697-87c4a655019f';
 export const MODAL_CLOSE_SEC = 2.5;
